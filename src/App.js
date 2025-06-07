8a999a77a08cce154ea356ba21e32809627f3cbf
mastermindccr <mastermindccr@mastermindccr-Ubuntu-24.04-PC>
2025-06-08 01:46:52 +0800

    [feat] Deploay dev version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import YoutubeConverter from './pages/YoutubeConverter';
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <Header/>
+        <div className="content-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/converter" element={<YoutubeConverter />} />
+          </Routes>
+        </div>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
